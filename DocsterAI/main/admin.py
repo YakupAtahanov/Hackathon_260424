0fbe4265abceba20705a7cca999e9e915c3f9335
sequery <agageldi.artykov@gmail.com>
2024-04-28 00:27:48 -0700

    Registered models Frameworks, Method, Class, and Variables in admin.py

--- DocsterAI/main/admin.py
+++ DocsterAI/main/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from main.models import Frameworks, Method, Class, Variables
 
 # Register your models here.
+admin.site.register(Frameworks)
+admin.site.register(Method)
+admin.site.register(Class)
+admin.site.register(Variables)
